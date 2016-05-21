a8a73c279d977bddd98a0bed33b690502e4234e1
deepakbharanikana <deepak_curly@yahoo.com>
2016-05-21 10:39:03 -0400

    Added custom toolbar

--- app/src/main/java/learninggist/com/bucketdrops/MainActivity.java
+++ app/src/main/java/learninggist/com/bucketdrops/MainActivity.java
@@ -2,15 +2,19 @@ package learninggist.com.bucketdrops;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Deepak on 5/21/16.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
