f79ab7f9ede065feb421cc0470812a756d3b26d8
sangram1985 <sangram.mohanty3@gmail.com>
2016-05-21 08:24:49 -0400

    The beginning of learningGist bucket drops practise project.

---
+++ app/src/main/java/learninggist/com/bucketdrops/MainActivity.java
@@ -0,0 +1,16 @@
+package learninggist.com.bucketdrops;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Deepak on 5/21/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
