f8a4dcf55da1f4fa44eabd5ce8392f3fa4607868
deepakbharanikana <deepak_curly@yahoo.com>
2016-05-21 09:38:13 -0400

    Design for Empty Layout

--- app/src/main/java/learninggist/com/bucketdrops/MainActivity.java
+++ app/src/main/java/learninggist/com/bucketdrops/MainActivity.java
@@ -1,12 +1,12 @@
 package learninggist.com.bucketdrops;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Deepak on 5/21/16.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
